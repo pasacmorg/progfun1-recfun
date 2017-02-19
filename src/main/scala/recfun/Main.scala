e1ec28350ac63136b7106030495e3f1e9893f8cf
psheets <psheets@zulily.com>
2017-02-19 12:07:48 -0800

    Initial commit

---
+++ src/main/scala/recfun/Main.scala
@@ -0,0 +1,32 @@
+package recfun
+
+object Main {
+  def main(args: Array[String]) {
+    println("Pascal's Triangle")
+    for (row <- 0 to 10) {
+      for (col <- 0 to row)
+        print(pascal(col, row) + " ")
+      println()
+    }
+  }
+
+  /**
+   * Exercise 1
+   */
+    def pascal(c: Int, r: Int): Int = {
+      if (c == 1 || c == r) 1
+      def pc(c: Int, r: Int): Int = {
+
+      }
+    }
+  
+  /**
+   * Exercise 2
+   */
+    def balance(chars: List[Char]): Boolean = ???
+  
+  /**
+   * Exercise 3
+   */
+    def countChange(money: Int, coins: List[Int]): Int = ???
+  }
